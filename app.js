cb3d91430351fdfa26a7e7f41901669315e479d2
Enrique Aspera <enrique.alejandro.aspera@gmail.com>
2022-09-26 09:43:19 +0100

    Built the main grid using CSS grid

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const container = document.querySelector('#container');
+const container = document.getElementById('container');
+
+function makeRows(rows, columns) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let grid = document.createElement('div');
+        container.appendChild(grid).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
